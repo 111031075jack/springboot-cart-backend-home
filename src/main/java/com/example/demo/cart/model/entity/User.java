7edf9286de08ace67ff1544b906bd1af9e5cd3bb
111031075jack <111031075@live.asia.edu.tw>
2025-10-01 20:20:20 +0800

    update

--- src/main/java/com/example/demo/cart/model/entity/User.java
+++ src/main/java/com/example/demo/cart/model/entity/User.java
@@ -9,15 +9,15 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "`user`")
 public class User {
-
+	
 	private Long id;
 	
 	private String username;
 	
-	private String password;
+	private String Password;
 }
