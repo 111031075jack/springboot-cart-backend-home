87d9df04f295af1fc2f2f701789e0406f1652d60
111031075jack <111031075@live.asia.edu.tw>
2025-10-01 20:10:33 +0800

    update

---
+++ src/main/java/com/example/demo/cart/model/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.demo.cart.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "`user`")
+public class User {
+
+	private Long id;
+	
+	private String username;
+	
+	private String password;
+}
