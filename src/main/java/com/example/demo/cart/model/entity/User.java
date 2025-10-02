30983a900b0af6a6045fbd9b7cfed49a66dfc709
111031075jack <111031075@live.asia.edu.tw>
2025-10-03 00:54:11 +0800

    update

--- src/main/java/com/example/demo/cart/model/entity/User.java
+++ src/main/java/com/example/demo/cart/model/entity/User.java
@@ -1,6 +1,10 @@
 package com.example.demo.cart.model.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,10 +18,13 @@ import lombok.Setter;
 @Entity
 @Table(name = "`user`")
 public class User {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column(length = 50, unique = true, nullable = false)
 	private String username;
 	
+	@Column(length = 50, nullable = false)
 	private String Password;
 }
