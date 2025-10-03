f56ab010fe73de6092301fa9981f4ad249831e50
111031075jack <111031075@live.asia.edu.tw>
2025-10-03 20:10:45 +0800

    update

---
+++ src/test/java/com/example/demo/AddUser.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.cart.model.entity.User;
+import com.example.demo.cart.repository.UserRepository;
+
+@SpringBootTest
+public class AddUser {
+
+	@Autowired
+	UserRepository userRepository;
+	
+	@Test
+	void add() {
+		User user1 = new User();
+		user1.setUsername("John");
+		user1.setPassword("1234");
+		
+		User user2 = new User();
+		user2.setUsername("Mary");
+		user2.setPassword("5678");
+		
+		userRepository.save(user1);
+		userRepository.save(user2);
+		
+		System.out.println("Add User Ok!");
+		
+	}
+	
+	
+}
