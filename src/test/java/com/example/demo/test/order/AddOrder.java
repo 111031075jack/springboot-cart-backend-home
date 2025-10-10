037123e71d0956a8eb32b63556a9af6cb9de3e92
111031075jack <111031075@live.asia.edu.tw>
2025-10-10 15:14:32 +0800

    update

---
+++ src/test/java/com/example/demo/test/order/AddOrder.java
@@ -0,0 +1,46 @@
+package com.example.demo.test.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.cart.model.entity.Order;
+import com.example.demo.cart.model.entity.OrderItem;
+import com.example.demo.cart.repository.OrderItemRepository;
+import com.example.demo.cart.repository.OrderRepository;
+
+@SpringBootTest
+public class AddOrder {
+
+	@Autowired
+	OrderRepository orderRepository;
+	
+	@Autowired
+	OrderItemRepository orderItemRepository;
+	 
+	@Test
+	public void add() {
+		// 建立訂單-主檔
+		Order order = new Order();
+		// 儲存訂單主檔
+		orderRepository.save(order);
+		
+		// 建立訂單-明細
+		OrderItem item1 = new OrderItem();
+		item1.setQty(10);
+		item1.setOrder(order); // 建立關聯
+		
+		OrderItem item2 = new OrderItem();
+		item2.setQty(20);
+		item2.setOrder(order); // 建立關聯
+		
+		// 儲存訂單項目
+		orderItemRepository.save(item1);
+		orderItemRepository.save(item2);
+		
+		System.out.println("Order Add OK !");
+		
+	}
+	
+	
+}
---
+++ src/test/java/com/example/demo/test/order/AddOrderItem.java
@@ -0,0 +1,5 @@
+package com.example.demo.test.order;
+
+public class AddOrderItem {
+
+}
