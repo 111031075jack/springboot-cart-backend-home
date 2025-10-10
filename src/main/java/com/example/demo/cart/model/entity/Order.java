218c0af2037359cb1fafd8e7e0af603127bb0370
111031075jack <111031075@live.asia.edu.tw>
2025-10-10 14:46:03 +0800

    update

---
+++ src/main/java/com/example/demo/cart/model/entity/Order.java
@@ -0,0 +1,30 @@
+package com.example.demo.cart.model.entity;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "`order`")
+public class Order {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	// FetchType.EAGER 查找 order 的同時會一並查找 orderItem
+	// FetchType.LAZY 查找 order 的同時不會一並查找 orderItem
+	@OneToMany(mappedBy = "order", fetch = FetchType.EAGER)
+	private List<OrderItem> orderItems;
+	
+}
