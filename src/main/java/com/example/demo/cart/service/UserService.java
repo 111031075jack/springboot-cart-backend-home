539a08d7c6a6bb0173d9d1092dcb1f13959f0145
111031075jack <111031075@live.asia.edu.tw>
2025-10-12 14:28:53 +0800

    update

---
+++ src/main/java/com/example/demo/cart/service/UserService.java
@@ -0,0 +1,20 @@
+package com.example.demo.cart.service;
+
+public interface UserService {
+	// 根據名字找使用者
+	
+	
+	// 登入
+	
+	
+	// 儲存
+	
+	
+	// 用戶關注列表(用戶關注哪些商品) - 正向查詢
+	
+	
+	// 商品關注列表(哪些商品被用戶關注) - 反向查詢
+	
+	
+	
+}
